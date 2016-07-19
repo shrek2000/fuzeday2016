4ac751b9dc7be29ad111aab8344d84f4d9a256b0
Pinhas Yakubov <pinhas.yakubov@teamaol.com>
2016-07-19 16:05:47 +0300

    clean HelloGmail

--- src/main/java/dev/priority/gmail/HelloGmail.java
+++ src/main/java/dev/priority/gmail/HelloGmail.java
@@ -1,20 +1,11 @@
 package dev.priority.gmail;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
-
-
-
 public class HelloGmail {
-
-
-
     public static void main(String[] args) throws Exception {
         SpringApplication.run(HelloGmail.class, args);
-
-
     }
 }
 
