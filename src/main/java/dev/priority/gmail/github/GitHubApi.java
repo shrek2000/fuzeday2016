5d2fb758d9f82f3083fe50563298227489bf7e1d
shirel azulay <10shirel@gmail.com>
2016-07-19 13:00:57 +0300

    Communication between Gmail to GitHub

--- src/main/java/dev/priority/gmail/github/GitHubApi.java
+++ src/main/java/dev/priority/gmail/github/GitHubApi.java
@@ -9,9 +9,9 @@ import org.springframework.web.client.RestTemplate;
 public class GitHubApi {
     private static final Logger log = LoggerFactory.getLogger(GitHubApi.class);
 
-    public String getUserData(String owner, String repo, int number) {
+    public String getUserData(String owner, String repositoryName, int numberPoolRequest) {
         RestTemplate restTemplate = new RestTemplate();
-        String url = "https://api.github.com/repos/" + owner + "/" + repo + "/pulls/" + number + "?access_token=9c3d0d236afd211a7955d7363c164136a84af76e";
+        String url = "https://api.github.com/repos/" + owner + "/" + repositoryName + "/pulls/" + numberPoolRequest + "?access_token=9c3d0d236afd211a7955d7363c164136a84af76e";
         GitHabData gitHabData = restTemplate.getForObject(url, GitHabData.class);
         log.info(gitHabData.toString());
         return gitHabData.getBody();
