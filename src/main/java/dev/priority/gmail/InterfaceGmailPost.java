69e99e0cc29817222bff7fea37332489ed32a417
yoavbenishai <yoav.ben.ishai@trackimo.com>
2016-07-19 11:32:27 +0300

    adding username and password

--- src/main/java/dev/priority/gmail/InterfaceGmailPost.java
+++ src/main/java/dev/priority/gmail/InterfaceGmailPost.java
@@ -7,7 +7,7 @@ import dev.priority.util.OnCompleteListener;
  * Created by yoavbenishai on 19-Jul-16.
  */
 public interface InterfaceGmailPost {
-    public void init(OnCompleteListener<Void> onCompleted);
+    public void init(String user, String password, OnCompleteListener<Void> onCompleted);
     public void register(OnCompleteListener<EmailMessage> onReceivedListenet);
     public void destroy(OnCompleteListener<Void> onCompleted);
 }
