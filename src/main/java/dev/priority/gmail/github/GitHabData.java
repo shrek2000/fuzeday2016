2975dcaf300873d2c1b7fe45c39327861933da61
Pinhas Yakubov <pinhas.yakubov@teamaol.com>
2016-07-19 12:08:14 +0300

    Add GitHubApi

---
+++ src/main/java/dev/priority/gmail/github/GitHabData.java
@@ -0,0 +1,49 @@
+package dev.priority.gmail.github;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GitHabData {
+    String url;
+    String id;
+    String body;
+
+    public GitHabData() {
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public GitHabData setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public GitHabData setBody(String body) {
+        this.body = body;
+        return this;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public GitHabData setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "GitHabData{" +
+                "body='" + body + '\'' +
+                ", url='" + url + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+}
